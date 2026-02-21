dea893e842c513a36d7ce0130131afab01b5b199
Ralf Wondratschek <ralf@squareup.com>
2026-02-20 21:09:32 -0800

    Broken test

--- compiler/src/test/resources/box/contributesservice/replacedServiceSingleCompilation.kt
+++ compiler/src/test/resources/box/contributesservice/replacedServiceSingleCompilation.kt
@@ -1,5 +1,6 @@
 package com.test
 
+import com.squareup.api.RealService
 import com.squareup.api.RetrofitAuthenticated
 import com.squareup.api.ServiceCreator
 import com.squareup.development.FakeMode
@@ -17,6 +18,9 @@ class FakeMyService : MyService
 interface MyGraph {
   val myService: MyService
 
+  @RealService
+  val realService: MyService
+
   val fakeService: FakeMyService
 
   @Provides @RetrofitAuthenticated
@@ -32,10 +36,12 @@ fun box(): String {
   var graph = createGraphFactory<MyGraph.Factory>().create(fake = true)
   assertTrue(graph.myService is FakeMyService, "Expected FakeMyService")
   assertTrue(graph.fakeService is FakeMyService)
+  assertTrue(graph.realService !is FakeMyService)
 
   graph = createGraphFactory<MyGraph.Factory>().create(fake = false)
   assertTrue(graph.myService !is FakeMyService, "Expected real MyService")
   assertTrue(graph.fakeService is FakeMyService)
+  assertTrue(graph.realService !is FakeMyService)
 
   return "OK"
 }
